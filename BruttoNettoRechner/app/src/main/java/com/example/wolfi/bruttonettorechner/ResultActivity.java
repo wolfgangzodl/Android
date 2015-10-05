5cda0da148df17857e4edc5f05c011632b16092a
wolfgangzodl <zodlwolfgang@gmail.com>
2015-10-05 12:13:34 +0200

    neue Activity angelegt
    neue Activity angelegt fuer die Ausgabe und ebenfalls erledigt ist
    auch, dass man beim RadioButton nur einen der beiden Button auswaehlen
    kann.

--- BruttoNettoRechner/app/src/main/java/com/example/wolfi/bruttonettorechner/ResultActivity.java
+++ BruttoNettoRechner/app/src/main/java/com/example/wolfi/bruttonettorechner/ResultActivity.java
@@ -1,24 +1,22 @@
 package com.example.wolfi.bruttonettorechner;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class ResultActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_result);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_result, menu);
         return true;
     }
 
@@ -35,10 +33,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void changeActivity(View view) {
-        Intent i = new Intent(MainActivity.this, ResultActivity.class);
-        startActivity(i);
     }
 }
